ab77fe3d9cd40a17cf3abd193026968e19a304d6
SzymonMs <szymon.murawski@student.put.poznan.pl>
2021-08-26 20:18:18 +0200

    Czujnik wilgotności
    Dodano czujnik wilgotności i ciśnienia DHT11

--- Czunik_cisnienia_i_temp.ino
+++ Czunik_cisnienia_i_temp.ino
@@ -4,30 +4,48 @@
 #include <U8g2lib.h>
 #include <SPI.h>
 #include <Adafruit_BMP085.h>
+#include "DHT.h" 
+       
+#define DHTPIN 2          
 
+DHT dht;  
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0);  
 Adafruit_BMP085 bmp;
  
 void setup()
 {
   u8g2.begin();
+  dht.setup(DHTPIN);    
   if (!bmp.begin())
-  {
-    while (1) {}
-  }
+  {while (1){}}
 }
  
 void loop(void){
+  
+delay(dht.getMinimumSamplingPeriod());
+
+int h = dht.getHumidity();  
 float temp =bmp.readTemperature();
-float pres=bmp.readPressure()/100;
-u8g2.clearBuffer(); // clear the internal memory
-u8g2.setFont(u8g2_font_t0_13b_mr); // set fonts
-u8g2.setCursor(10,10); // set cursor position
-u8g2.print(temp); // write something to the internal memory
-u8g2.print(" *C"); // write something to the internal memory
-u8g2.setCursor(10,30); // set cursor position
-u8g2.print(pres); // write something to the internal memory
-u8g2.print(" hPa"); // write something to the internal memory
-u8g2.sendBuffer(); // transfer internal memory to the display
+int pres=bmp.readPressure()/100;
+
+u8g2.clearBuffer();
+u8g2.setFont(u8g2_font_t0_13b_mr);
+
+//Pomiar temperatury
+u8g2.setCursor(5,10);
+u8g2.print(temp);
+u8g2.print(" *C");
+//Pomiar wilgotności
+u8g2.setCursor(70,10);
+u8g2.print(" RH ");
+u8g2.print(h);
+u8g2.print("%");
+//Pomiar ciśnienia
+u8g2.setCursor(5,30);
+u8g2.print(pres);
+u8g2.print(" hPa");
+
+u8g2.sendBuffer();
+
 delay(2000);
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# Meteo
-Simple project using Arduino UNO, MBP180 pressure sensor and OLED display.
